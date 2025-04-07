b76fa61822e7e81859d8b57a1bbae7ce5a7356e6
zhangweidong02 <zhangweidong02@zuoyebang.com>
2025-04-07 11:42:56 +0800

    add trie pdf_cpu testcase

--- mypdfcpu/mypdfcpu.go
+++ mypdfcpu/mypdfcpu.go
@@ -1,18 +1,25 @@
 package mypdfcpu
 
 import (
+	"fmt"
 	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"time"
 )
 
-func MergePdf() {
+func MergePdf(inFilePath, outFilePath string, cnt int) {
 	// 输入 PDF 文件列表
-	inFiles := []string{"/Users/zyb/code/mycode/go-study/mypdfcpu/test1.pdf", "/Users/zyb/code/mycode/go-study/mypdfcpu/mutex.pdf"}
+	inFiles := []string{}
+	for i := 0; i < cnt; i++ {
+		inFiles = append(inFiles, inFilePath)
+	}
 
 	// 输出合并后的 PDF 文件
-	outFile := "/Users/zyb/code/mycode/go-study/mypdfcpu/merged.pdf"
+	outFile := outFilePath
 
+	start := time.Now()
 	// 合并操作
 	if err := api.MergeCreateFile(inFiles, outFile, true, nil); err != nil {
 		panic(err)
 	}
+	fmt.Println(time.Since(start))
 }
--- mypdfcpu/mypdfcpu_test.go
+++ mypdfcpu/mypdfcpu_test.go
@@ -7,6 +7,20 @@ import (
 
 func TestMergePdf(t *testing.T) {
 	convey.Convey("test merge", t, func() {
-		MergePdf()
+		//MergePdf("/Users/zyb/code/mycode/go-study/mypdfcpu/test1.pdf",
+		//	"/Users/zyb/code/mycode/go-study/mypdfcpu/merged-90.pdf", 22)
+		//
+		//MergePdf("/Users/zyb/code/mycode/go-study/mypdfcpu/11月分享会-部分内容.pdf",
+		//	"/Users/zyb/code/mycode/go-study/mypdfcpu/merged-36.pdf", 22)
+		i := 0
+		for {
+			if i >= 10 {
+				break
+			}
+			MergePdf("/Users/zyb/code/mycode/go-study/mypdfcpu/第 1 讲抽象概括能力提升（一） (1).pdf",
+				"/Users/zyb/code/mycode/go-study/mypdfcpu/merged-1.pdf", 22)
+			i++
+		}
+
 	})
 }
